3f5a738929ec16ca40806fc8d64b5e7ad1ae87fa
firat <firat.duzgun@hotmail.com>
2024-10-02 17:36:11 +0300

    design basic html/css properties

---
+++ index.js
@@ -0,0 +1,13 @@
+// calculator will include add, substract, multiply, divide ... basic functions
+
+// function input will be: a number, an operator and another number => 3 variables
+
+// operator will be a function that takes
+
+// create buttons for each digit,operators, equal sign,clear a backspace, decimal point with HMTL
+
+// create a display value function to display clicked buttons value
+
+// Your calculator should not evaluate more than a single pair of numbers at a time. Example: you press a number button (12), followed by an operator button (+), a second number button (7), and finally a second operator button (-). Your calculator should then do the following: first, evaluate the first pair of numbers (12 + 7), second, display the result of that calculation (19), and finally, use that result (19) as the first number in your new calculation, along with the next operator (-).
+
+// keyboard support
